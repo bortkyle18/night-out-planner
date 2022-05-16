2a93bd567d65b3c5c6c39812799306c00363c80b
bortkyle18 <bortkyle18@gmail.com>
2022-05-16 19:43:58 -0400

    food api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,85 +76,84 @@ var eventLi = function() {
 
 
 
-
-// Find food and print to page with links
+// Find food and print to page
 var foodLi = function() {
     // clear list
     $("#foodLi").empty()
+    var cityEnter = $("#city").val();
 
-    var foodHeader = $("<h1>")
-        .text("Restuarants")
-        .addClass("text-center");
+    function searchApi(cityEnter) {
+        var queryUrl =
+            "https://cors-anywhere.herokuapp.com/" +
+            "https://api.yelp.com/v3/businesses/search?restaurants&location=" +
+            cityEnter +
+            "&limit=50";
+        fetch(queryUrl, {
+            method: "GET",
+            headers: {
+            Accept: "application/json",
+            Authorization:
+                "Bearer " +
+                "67mqRNvaaHn5QdfWrCTna5d97oAiv8E-L2rhNIQ0CfS9TIqsqV-dmw9UJJ6KmHlWW_Od--P0iRNlN3ujxHB0ILtCZGR-sKAUtkGgRXIf7MZqihLIq8E3_9kluVJ8YnYx",
+                "Content-Type": "application/json",
+            },
+        }).then(function (response) {
+            response.json().then(function (data) {
+                console.log(data)
+            if (data.total === 0) {
+                noFoodFound()
+            } else {
+                foodFound(data.businesses);
+            };
+            });
+        });
+    };
+
+    // Create HTML for events to populate list
+    var foodFound = function(food) {
+        var foodHeader = $("<h1>")
+            .text("Food")
+            .addClass("text-center");
+        $("#foodLi").append(foodHeader);
+        console.log(food)
+
+        for(var i = 0; i < food.length; i++) {
+            var foodList = $("<li>")
+            .addClass("callout primary small");
+            var foodName = $("<a>")
+                .attr('target','_blank')
+                .attr("href",food[i].url)
+                .text(food[i].name)
+                .css("font-weight","Bold");
+            var foodType = $("<p>")
+                .text(food[i].categories[0].title);
+            var foodPrice = $("<p>")
+                .text(food[i].price);
+
+            foodList.append(foodName,foodType,foodPrice);
+    
+            $("#foodLi").append(foodList);
+        };
+    };
+
+    var noFoodFound = function() {
+        var foodHeader = $("<h2>")
+            .text("WARNING: Restuarants Not Found")
+            .addClass("text-center notFound");
         $("#foodLi").append(foodHeader);
 
-    var foodList = $("<li>")
-        .addClass("callout primary small");
-    var foodTest = $("<p>")
-        .text("Food list will go here");
-    foodList.append(foodTest);
-    
-    $("#foodLi").append(foodList);}
-//     // Create HTML for events to populate list
-//     var foodFound = function(food) {
-//         for(var i = 0; i < food.length; i++) {
-//             var foodList = $("<li>")
-//             .addClass("callout primary small");
-//             var foodName = $("<a>")
-//                 .attr('target','_blank')
-//                 .attr("href",food[i].url)
-//                 .text(food[i].name)
-//                 .css("font-weight","Bold");
-//             var foodLocation = $("<p>")
-//                 .text(food[i].location);
-//             var foodType = $("<p>")
-//                 .text(food[i].type);
+        var foodList = $("<li>")
+            .addClass("alert-box callout alert");
+        var noFoodFound = $("<p>")
+            .text("Oh no! There are no restuarants found in the city provided. Please check the spelling of the city you entered or search cities around your area to find an event for you!");
+        foodList.append(noFoodFound);
 
-//             foodList.append(foodName,foodType,foodLocation);
-    
-//         $("#foodLi").append(foodList);
-//         };
-//     };
+        $("#foodLi").append(foodList);
+    };
 
-//     var noFoodFound = function() {
-//         var foodList = $("<li>")
-//         var noFoodFound = $("<h2>")
-//             .text("Oh no! There are no restaurants in the city provided. Please check the spelling of the city you entered or search cities around your area to find a restuarant for you!");
-
-//         foodList.append(noFoodFound);
-
-//         $("#foodLi").append(foodList);
-//     };
-
-//     var getEventRepo = function() {
-
-//         // Get entered city
-//         var cityEnter = $("#city").val();
-//         // pull up to 200 responses from the requested city - sorted by date of event
-//         var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=200&city=" + cityEnter + "&sort=date,asc&apikey=syoZ02A5zn7KOdclO7ZeAyKBCV7U8DK7";
-    
-//         $.ajax({
-//             type:"GET",
-//             url: apiUrl,
-//             async:true,
-//             dataType: "json",
-//             success: function(json) {
-//                 if (json.page.totalElements === 0) {
-//                     noFoodFound()
-//                 } else {
-//                     foodFound(json._embedded.events);
-//                 };
-//             },
-    
-//         });
-//     };
-//     getEventRepo();
-// };
-// End of food code
-
-// Find food and print to page with links
-var getYelpAPI = function() {
-    fetch("https://api.yelp.com/v3/businesses/search");
+    searchApi(cityEnter)
 };
+// End of food code
 
 
 
